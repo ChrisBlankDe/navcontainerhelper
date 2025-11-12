c63e45a392465768ab3e2ef980b04b8866408579
Chris Blank <github@chris-blank.de>
2025-11-12 16:14:55 +0100

    improve deciding IsBcSaaSInfrastructure

--- AppHandling/Run-AlPipeline.ps1
+++ AppHandling/Run-AlPipeline.ps1
@@ -838,7 +838,8 @@ if ($bcptTestFolders) {
 
 $artifactUrl = ""
 $filesOnly = $false
-if ($bcAuthContext) {
+$IsBcSaaSInfrastructure = $bcAuthContext.PSObject.Properties.Name -contains 'scopes' -and $bcAuthContext.scopes -in @('https://api.businesscentral.dynamics.com/','https://projectmadeira.com/')
+if ($IsBcSaaSInfrastructure) {
     if ("$environment" -eq "") {
         throw "When specifying bcAuthContext, you also have to specify the name of the pre-setup online environment to use."
     }
